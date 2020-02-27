63984b3b366c92d5ba6f40574cc01d04530dc98f
Andrea <aos26@hi.is>
2020-02-27 09:14:27 +0000

    first commit

---
+++ app/src/main/java/is/hi/hbv601g/icelandictutor/MainActivity.java
@@ -0,0 +1,27 @@
+package is.hi.hbv601g.icelandictutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLoginButton = findViewById(R.id.loginButton);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
