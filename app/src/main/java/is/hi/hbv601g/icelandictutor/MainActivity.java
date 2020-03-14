bec62df23d5421b0be260f207e6685334f1bb1ef
aos26 <aos26@hi.is>
2020-03-14 21:33:48 +0000

    tenging við db

--- app/src/main/java/is/hi/hbv601g/icelandictutor/MainActivity.java
+++ app/src/main/java/is/hi/hbv601g/icelandictutor/MainActivity.java
@@ -4,8 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +28,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        String usersUrl = "https://icelandic-tutor.herokuapp.com/users";
+        String url = "https://icelandic-tutor.herokuapp.com/questions?cat_id=1&lvl_id=1";
+
+        JsonArrayRequest objectRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.e("Rest response", response.toString());
+                        TextView txtView = findViewById(R.id.UserNameTest);
+                        //txtView.setText(response.toString());
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject jsonobject = null;
+                            try {
+                                jsonobject = response.getJSONObject(i);
+                                String answer = jsonobject.getString("answer");
+                                txtView.setText(answer);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest error", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
 
         mLoginButton = findViewById(R.id.loginButton);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
