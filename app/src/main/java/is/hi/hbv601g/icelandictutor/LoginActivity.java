704cd0f9cc5473e837ea47527c9424a9b2ac7890
leajerman <ljp1@hi.is>
2020-03-16 16:47:56 +0000

    tenging fra login i main

--- app/src/main/java/is/hi/hbv601g/icelandictutor/LoginActivity.java
+++ app/src/main/java/is/hi/hbv601g/icelandictutor/LoginActivity.java
@@ -1,6 +1,8 @@
 package is.hi.hbv601g.icelandictutor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void logIn(View view) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
